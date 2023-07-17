3cb7623256da3078a3c580bbe4fba4e224167be1
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2023-07-17 15:10:46 +0900

    Update primetest_factoring.cpp

--- primetest_factoring.cpp
+++ primetest_factoring.cpp
@@ -1,81 +1,69 @@
-//Miller-Rabin / Pollard rho
-#pragma GCC optimize("Ofast")
-#include<bits/stdc++.h>
-using namespace std;
-bool isPrime(long long x){
-	assert(x>0);
-	if(x==1)
-		return false;
-	long long _x=x-1;
-	int r=0;
-	for(;_x%2==0;r++)
-		_x/=2;
-	for(int a:{2,3,5,7,11,13,17,19,23,29,31,37})
-		if(x!=a){
-			__int128 rp=1,r_=a%x;
-			for(long long i=1;i<=_x;i*=2){
-				if(i&_x)
-					rp=rp*r_%x;
-				r_=r_*r_%x;
-			}
-			if(rp==1||rp==x-1)
-				continue;
-			for(int i=1;i<r;i++){
-				rp=rp*rp%x;
-				if(rp==x-1)
-					break;
-			}
-			if(rp!=x-1)
-				return false;
+//from DeobureoMinkyuParty
+namespace miller_rabin{
+    lint mul(lint x, lint y, lint mod){ return (__int128) x * y % mod; }
+	lint ipow(lint x, lint y, lint p){
+		lint ret = 1, piv = x % p;
+		while(y){
+			if(y&1) ret = mul(ret, piv, p);
+			piv = mul(piv, piv, p);
+			y >>= 1;
 		}
-		else
-			return true;
-	return true;
-}
-vector<long long> V;
-void f(long long n){
-	if(isPrime(n)){
-		V.push_back(n);
-		return;
+		return ret;
 	}
-	for(int i=2;;i++){
-		__int128 x=i,y=i;
-		for(;;){
-			x=(x*x+1)%n;
-			y=(y*y+1)%n;
-			y=(y*y+1)%n;
-			__int128 _x=x>y?x-y:y-x,_y=n;
-			if(_x<_y)
-				swap(_x,_y);
-			while(_y){
-				_x%=_y;
-				swap(_x,_y);
-			}
-			if(_x!=1)
-				if(_x==n)
-					break;
-				else{
-					V.push_back(_x);
-					f(n/_x);
-					return;
-				}
+	bool miller_rabin(lint x, lint a){
+		if(x % a == 0) return 0;
+		lint d = x - 1;
+		while(1){
+			lint tmp = ipow(a, d, x);
+			if(d&1) return (tmp != 1 && tmp != x-1);
+			else if(tmp == x-1) return 0;
+			d >>= 1;
 		}
 	}
-}
-int main(){
-	cin.tie(0);
-	ios::sync_with_stdio(false);
-	long long n;
-	cin>>n;
-	for(int i=2;i<1234567;i++){
-		while(n%i==0){
-			V.push_back(i);
-			n/=i;
+	bool isprime(lint x){
+		for(auto &i : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}){
+			if(x == i) return 1;
+			if(x > 40 && miller_rabin(x, i)) return 0;
 		}
+		if(x <= 40) return 0;
+		return 1;
 	}
-	if(n!=1)
-		f(n);
-	sort(V.begin(),V.end());
-	for(auto i:V)
-		cout<<i<<'\n';
 }
+
+namespace pollard_rho{
+	lint f(lint x, lint n, lint c){
+		return (c + miller_rabin::mul(x, x, n)) % n;
+	}
+	void rec(lint n, vector<lint> &v){
+		if(n == 1) return;
+		if(n % 2 == 0){
+			v.push_back(2);
+			rec(n/2, v);
+			return;
+		}
+		if(miller_rabin::isprime(n)){
+			v.push_back(n);
+			return;
+		}
+		lint a, b, c;
+		while(1){
+			a = rand() % (n-2) + 2;
+			b = a;
+			c = rand() % 20 + 1;
+			do{
+				a = f(a, n, c);
+				b = f(f(b, n, c), n, c);
+			}while(gcd(abs(a-b), n) == 1);
+			if(a != b) break;
+		}
+		lint x = gcd(abs(a-b), n);
+		rec(x, v);
+		rec(n/x, v);
+	}
+	vector<lint> factorize(lint n){
+		vector<lint> ret;
+		rec(n, ret);
+		sort(ret.begin(), ret.end());
+		return ret;
+	}
+};
